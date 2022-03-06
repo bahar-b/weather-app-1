10b6c2034dd471ed37330d741949d91c404cd7dd
bahar-b <bahar.balalzadeh@gmail.com>
2022-03-06 14:30:54 +0330

    function of navigator

--- src/app.js
+++ src/app.js
@@ -69,8 +69,7 @@ function showCurrentLoc(position) {
   let apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyCurrent}&units=metric`;
   axios.get(apiUrlCurrent).then(showCurrent);
 }
-function getCurrentLocation(event) {
-  event.preventDefault();
+function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(showCurrentLoc);
 }
 
